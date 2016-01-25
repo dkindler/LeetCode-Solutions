a9ebc08c840c46158e0e0e58869425b35a479a50
Daniel Kindler <DanielKindler@Dans-Mac.local>
2016-01-24 22:10:46 -0500

    performance updates to problem 3

--- 3.java
+++ 3.java
@@ -1,25 +1,30 @@
 // https://leetcode.com/problems/longest-substring-without-repeating-characters/
 public class Solution {
     public int lengthOfLongestSubstring(String s) {
-        HashMap<Character, Integer> map = new HashMap<>();
+        int max = 0;
+        int cur = 0;
 
-        int maxLength = 0;
-        int curLength = 0;
+        HashMap<Character, Integer> map = new HashMap<>();
 
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-
             if (map.containsKey(c)) {
-                maxLength = Math.max(maxLength, curLength);
-                curLength = 0;
-                i = map.get(c);
-                map = new HashMap<>();
+
+                if ((i - cur) > map.get(c)) {
+                    map.put(c, i);
+                    cur++;
+                } else {
+                    cur = i - map.get(c);
+                    map.put(c, i);
+                }
             } else {
                 map.put(c, i);
-                maxLength = Math.max(maxLength, ++curLength);
+                cur++;
             }
+
+            max = Math.max(cur, max);
         }
 
-        return maxLength;
+        return max;
     }
 }
